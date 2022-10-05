9534ded762362eea3576b6c4df322bc97894273a
vishy08 <vishyk@live.unc.edu>
2022-10-05 10:16:20 -0400

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ import requests, json
 from datetime import datetime
 import certifi
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://vishy08:Avador25@cluster0.sph8g3m.mongodb.net/?retryWrites=true&w=majority", tlsCAFile = certifi.where())
+client = MongoClient("mongodb+srv://vishy08:Avador25@cluster0.sgqoke3.mongodb.net/test", tlsCAFile = certifi.where())
 db = client["mydb"]
 
 app = Flask(__name__)
